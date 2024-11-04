0388f8dfa6d64a2fde3450037bf413e45269e2ac
chiragkumawat <chiragkumawat457@gmail.com>
2024-11-04 22:00:26 +0530

    Movie_Zone

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Movie from './MoviesZone/Movie'
+
+const App = () => {
+  return (
+    <>
+     <div>
+        <Movie></Movie>
+    </div>
+    </>
+   
+  )
+}
+
+export default App
